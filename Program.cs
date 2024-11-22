cc07c3d0ffe709d7e8ce6f25c6f0a4445c40c4fd
JordiMena <jjyordy78@gmail.com>
2024-11-22 18:53:59 -0400

    Creacion de interfaz
    Se creo la interfaz del programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+    Console.WriteLine(@"
+
+        ╔╗ ┬┌─┐┌┐┌┌┐ ┌─┐┌┐┌┬┌┬┐┌─┐  ┌─┐┬    ╔═╗┬┌─┐┌┬┐┌─┐┌┬┐┌─┐
+        ╠╩╗│├┤ │││├┴┐├┤ ││││ │││ │  ├─┤│    ╚═╗│└─┐ │ ├┤ │││├─┤
+        ╚═╝┴└─┘┘└┘└─┘└─┘┘└┘┴─┴┘└─┘  ┴ ┴┴─┘  ╚═╝┴└─┘ ┴ └─┘┴ ┴┴ ┴
+
+
+        1.Determinar si un triangulo es Equilatero, Isoceles o O escaleno.
+        2.Tabla de multiplicar.
+        3.genero de una persona.
+        0.Salir
+    
+    ");
